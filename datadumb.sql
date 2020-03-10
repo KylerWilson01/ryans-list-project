cd04e9a6357bf764b3c76047a6a7a630eb16b9cd
Kyler Wilson <kyleraw2001@gmail.com>
2020-03-10 09:00:03 -0700

    sql file

---
+++ datadumb.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM categories.listings
+LIMIT 0, 1000
+
+-- Date: 2020-03-10 08:58
+*/
+INSERT INTO `` (`id`,`listingname`,`subcat_id`,`desc`,`price`,`city`,`state`,`time_created`) VALUES (1,'Shitty Car',102,'I hate this POS! please get it out of here',250,'Las Vegas','Nevada','2020-03-09 16:15:07');
+INSERT INTO `` (`id`,`listingname`,`subcat_id`,`desc`,`price`,`city`,`state`,`time_created`) VALUES (2,'Sweet Gaming rig',105,'I have this gaming setup but I can\'t play on it anymore due to work. Contact me for specs',600,'Henderson','Nevada','2020-03-09 16:15:57');
+INSERT INTO `` (`id`,`listingname`,`subcat_id`,`desc`,`price`,`city`,`state`,`time_created`) VALUES (3,'Jo',6,'I am Jo. I need friends.',0,'Las Vegas','Nevada','2020-03-09 16:16:53');
